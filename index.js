d8f25cc71dfb7b0f35e83330fd2924201a300cab
quang.ho <quang.ho@auvietsoft.vn>
2020-06-30 18:26:33 +0700

    using middleware to handling error and add methods: edit, delete, ..

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-require('dotenv').config()
-
-//start node server
+//using express to start init server
 const express = require('express')
 const app = express()
-app.use(express.json())
-const port = process.env.PORT
-app.listen(port, ()=>{
-  console.log(`Server running on port ${port}`)
-})
 
-
-//const Note = mongoose.model('Note', noteSchema)
+//using enviroment library
+require('dotenv').config()
+//init model of Note in mongodb
 const Note = require('./models/note')
+
+//middleware of express
+app.use(express.json())
 
 //
 //api
@@ -26,7 +23,7 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -36,14 +33,16 @@ app.get('/api/notes/:id', (request, response) => {
       }
     })
     .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
+      next(error);
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  response.send(id.toString());  
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error)) 
 })
 
 app.post('/api/notes', (request, response) => {
@@ -62,6 +61,50 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+})
+
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+
+
+//
+//error handling by middleware
+//
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: error.message })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
+//start server
+const port = process.env.PORT
+app.listen(port, ()=>{
+  console.log(`Server running on port ${port}`)
 })
 
 
